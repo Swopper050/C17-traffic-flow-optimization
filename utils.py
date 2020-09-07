bee906bdcd8b53f5f8eba839a175209a175c5564
Swopper050 <robocop@brams-mbp.home>
2020-09-07 15:55:32 +0200

    First (or second?) commit

---
+++ utils.py
@@ -0,0 +1,11 @@
+def get_traffic_light_nodes(G):
+    """
+    Given a graph (as loaded with osmnx), returns all nodes with traffic lights. This is done
+    using the degrees of the graph. If the degree is higher than 3, it is considered a traffic
+    light node.
+
+    :param G: Graph instance
+    :returns: list of traffic light nodes
+    """
+
+    return [node for node in G.degree() if node[1] > 3]
