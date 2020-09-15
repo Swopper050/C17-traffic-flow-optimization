3ed34e56c385f86d1309037f8ed560a1b4844f0c
Swopper050 <robocop@brams-mbp.home>
2020-09-15 19:04:44 +0200

    Low manhattan test

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ utn = list(set(utn + oxna + oxnt))
 utw = list(set(utw + oxwa + oxwt))
 ox.config(all_oneway=True, useful_tags_node=utn, useful_tags_way=utw)
 
-G = ox.graph_from_point((40.78343, -73.96625), dist=1000, network_type="drive")
+G = ox.graph_from_point((40.743382, -73.988689), dist=800, network_type="drive")
 # G = ox.graph_from_place("New York City, New York", network_type="drive")
 import pdb; pdb.set_trace()
