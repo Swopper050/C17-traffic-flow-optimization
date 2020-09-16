b108f14feda6d87a286bb5227ae987874dcbf353
adithya-ms <adithya.ms15@gmail.com>
2020-09-16 23:18:16 +0200

    Framework for creating agent objects and initialising them

--- agents.py
+++ agents.py
@@ -1,45 +1,72 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Sep 13 20:55:54 2020
-
-@author: 91782
+This file defines the agent class
 """
 import json
 import pdb
 import os
+from copy import copy
+
+
+#flow_file_path                 -Sample flow file for reference
+#agent_flow_file_path           -Flow file created based on simulation config
 
 flow_file_path =  ".\\test_sim\\flow.json"
 agent_flow_file_path =  ".\\test_sim\\agents_flow.json"
+
+#Class attributes are all derived from flow.json. Refer citiflow docs for description
 class vehicle:
-    vehicle_id = []
+	vehicle_id = []
     
-    def __init__(self, vehicle_prop):
-        self.length = vehicle_prop.length
-        self.width = vehicle_prop.width
-        self.maxPosAcc = vehicle_prop.maxPosAcc
-        self.maxNegAcc = vehicle_prop.maxNegAcc
-        self.usualPosAcc = vehicle_prop.usualPosAcc
-        self.usualNegAcc = vehicle_prop.usualNegAcc
-        self.minGap = vehicle_prop.minGap
-        self.maxSpeed = vehicle_prop.maxSpeed
-        self.headwayTime = vehicle_prop.headwayTime
-        self.route = vehicle_prop.route
-        self.interval = vehicle_prop.interval
-        self.startTime = vehicle_prop.startTime
-        self.endTime = vehicle_prop.endTime
-		
-def update_flow_json():
-	pdb.set_trace()
-	if os.path.exists(agent_flow_file_path):
-		with open(agent_flow_file_path) as f:
-			flow_json = json.load(f)
-	else:
-		if os.path.exists(flow_file_path):
-			with open(flow_file_path) as f:
+	def __init__(self, vehicle_prop):
+		self.length = vehicle_prop.length  
+		self.width = vehicle_prop.length / 2.5       # Maintaining aspect ratio as 2.5
+		self.maxPosAcc = vehicle_prop.maxPosAcc
+		self.maxNegAcc = vehicle_prop.maxNegAcc
+		self.usualPosAcc = vehicle_prop.maxPosAcc / 2.0  # Assuming usual acceleration is half of maximum acceleration
+		self.usualNegAcc = vehicle_prop.maxNegAcc / 2.0
+		self.minGap = vehicle_prop.minGap
+		self.maxSpeed = vehicle_prop.maxSpeed
+		self.headwayTime = vehicle_prop.headwayTime
+		self.route = copy(vehicle_prop.route)
+		self.interval = vehicle_prop.interval
+		self.startTime = vehicle_prop.startTime
+		self.endTime = vehicle_prop.endTime
+		self.update_flow_json()
+
+	def update_flow_json(self):
+		if os.path.exists(agent_flow_file_path):
+			with open(agent_flow_file_path) as f:
 				flow_json = json.load(f)
-			with open(agent_flow_file_path, 'w') as f:
-				json.dump(flow_json,f)
 		else:
-			raise Exception("Default flow file not found")
-	#with open(agent_flow_file_path) as f:
-			
+			flow_json = []
+
+		#Create a dict with class attributes and write to agents_flow.json
+		
+		new_entry = dict()
+		new_entry['vehicle'] = dict()
+		new_entry['vehicle']['length'] = self.length
+		new_entry['vehicle']['width'] = self.width
+		new_entry['vehicle']['maxPosAcc'] = self.maxPosAcc
+		new_entry['vehicle']['maxNegAcc'] = self.maxNegAcc
+		new_entry['vehicle']['usualPosAcc'] = self.usualPosAcc
+		new_entry['vehicle']['usualNegAcc'] = self.usualNegAcc
+		new_entry['vehicle']['minGap'] = self.minGap
+		new_entry['vehicle']['maxSpeed'] = self.maxSpeed
+		new_entry['vehicle']['headwayTime'] = self.headwayTime
+		new_entry['route'] = copy(self.route)
+		new_entry['interval'] = self.interval
+		new_entry['startTime'] = self.startTime
+		new_entry['endTime'] = self.endTime
+		
+		if new_entry not in flow_json:
+			flow_json.append(new_entry)
+		else:
+			print("Similar entry already present")
+		
+		with open(agent_flow_file_path, 'w+') as f:
+			json.dump(flow_json,f)
+
+		return
+	
+	
--- main_agent.py
+++ main_agent.py
@@ -1,11 +1,67 @@
+'''
+This is the main file from which agent objects can be created.
+'''
+
 import argparse
-from agents import vehicle, update_flow_json
+from agents import vehicle
 import pdb
+import random
+import types
+import json
+
+#Thisfunction generates random values for vehicle properties
+def create_random_vehicle(config):
+	sim_config = list()
+	with open("./test_sim/replay_roadnet.json") as f:
+		roadnet_json = json.load(f)
+
+	#Check if road ids are present in roadnet json
+	if ('static' not in roadnet_json) or  \
+		 ( 'edges' not in roadnet_json['static']) or \
+		 (len(roadnet_json['static']['edges']) < 2):
+		raise Exception("Roadnet.json does not sufficient data to initialise vehicles")
+
+	#Add road ids from roadnet json to route attribute of class
+	# Based on input, 2 or 3 road indices from roadnet json	are stored and added to self.route
+	for i in range(0,config.num_of_agents):
+		road_index = []
+		while(len(road_index) < config.anchor_points + 1):
+			random_index = random.randint(0,len(roadnet_json['static']['edges']) - 1)
+			if random_index in road_index:
+				pass
+			else:
+				road_index.append(random_index)
+		'''
+		Add code for random intersection
+		'''
+		#Create a namespace object to
+		temp_config = types.SimpleNamespace(
+			length = round(random.uniform(4,6),1),
+			#width = length / 2.5,
+			maxPosAcc = round(random.uniform(1,4),1),
+			maxNegAcc = round(random.uniform(4,7),1),
+			#usualPosAcc = maxPosAcc / 2,
+			#usualNegAcc = maxNegAcc / 2,
+			minGap = round(random.uniform(0.5,3),1),
+			maxSpeed = round(random.uniform(10,50),2),
+			headwayTime = round(random.uniform(1,3),1),
+			route = [ roadnet_json['static']['edges'][index]['id'] for index in road_index],
+			interval = 2.0,
+			startTime = 0,
+			endTime = -1
+			)
+		sim_config.append(temp_config) 
+	return sim_config
+	
 def main():
-    parser = argparse.ArgumentParser(
-        description="Create vehicle class objects"
-    )
-    parser.add_argument('-l', '--length',
+
+	parser = argparse.ArgumentParser(
+		description="Create vehicle class objects"
+		)
+	
+	'''
+	This section can be used if input is given manually and not randomly
+	parser.add_argument('-l', '--length',
                         type=float,
                         default=5.0,
                         help='Length of vehicle')
@@ -25,7 +81,7 @@ def main():
                         type=float,
                         default=2.0,
                         help='Usual acceleration of the vehicle')
-    parser.add_argument('-ud', '--usual_deceleration',
+    parser.add_argument('-ud', '--usual_dec',
                         type=float,
 						default=4.5,
 						help='Usual deceleration of the Vehicle')
@@ -57,10 +113,18 @@ def main():
                         type=list,
                         default=[],
                         help='Source, destination and anchor points to traverse')
+    '''
+	parser.add_argument('-n', '--num_of_agents',
+                        type=int,
+                        default=2,
+                        help='Number of distinct vehicles to generate')
+	parser.add_argument('-a', '--anchor_points',
+                        type=int,
+                        default=2,
+                        help="Source to destination route. Value '1': Shortest Path. Value '2': Through 1 randomly chosen road. Value '3': Through 1 randomly chosen intersection.")
+	config = parser.parse_args()
+	vehicle_config = create_random_vehicle(config)
+	vehicles = [ vehicle(vehicle_config[i]) for i in range(0,config.num_of_agents)]
 	
-    config = parser.parse_args()
-    update_flow_json()
-	
-
 if __name__ == '__main__':
 	main()
