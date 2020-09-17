93cb033ce6bd6e80482d713c58a550e587927cb3
Rik Vegter <rikvegter@MacBook-Pro-van-Rik.local>
2020-09-17 12:29:24 +0200

    test

--- test_make_sim.py
+++ test_make_sim.py
@@ -6,7 +6,13 @@ if __name__ == "__main__":
     parser.add_argument("--dir", type=str, default='low_manhattan_sim')
     args = parser.parse_args()
     eng = cityflow.Engine(f"{args.dir}/config.json", thread_num=1)
-    for _ in range(1000):
+    for i in range(100):
         vehicleCount = eng.get_vehicle_count()
-        print(vehicleCount)
+        waitingTimePerLane = eng.get_lane_waiting_vehicle_count()
+        averageTravelTime = eng.get_average_travel_time()
+
+        print('total vehicles in manhattan = ', vehicleCount)
+        print('Waiting time per lane = ', waitingTimePerLane)
+        print('Average travel time = ', averageTravelTime)
+
         eng.next_step()
