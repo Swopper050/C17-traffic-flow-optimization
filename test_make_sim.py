451affa825b1baf3e03501e8c6628df1072dea92
Rik Vegter <rikvegter@MacBook-Pro-van-Rik.local>
2020-09-17 20:24:36 +0200

    test

--- test_make_sim.py
+++ test_make_sim.py
@@ -20,13 +20,13 @@ if __name__ == "__main__":
         for vehicle in vehicleIDs:
             vehicleInfo = eng.get_vehicle_info(vehicle)
             #if vehicleInfo['running'] == 0 or vehicleInfo['running'] == 1:
-                route = vehicleInfo['running']['route']
-                #Create an array where every element is a lane_ID of the route of a car
-                route = route.split(' ')
-                print('\nroute = ', route, '\n')
-                print('vehicles per lane = ', waitingVehiclesPerLane, '\n')
-                for lane in route:
-                    if lane in waitingVehiclesPerLane.keys():
-                        print("We might have a conjegtion")
+            route = vehicleInfo['running']['route']
+            #Create an array where every element is a lane_ID of the route of a car
+            route = route.split(' ')
+            print('\nroute = ', route, '\n')
+            print('vehicles per lane = ', waitingVehiclesPerLane, '\n')
+            for lane in route:
+                if lane in waitingVehiclesPerLane.keys():
+                    print("We might have a conjegtion")
 
         eng.next_step()
