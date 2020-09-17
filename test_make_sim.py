5f611f99d87a629f9dab658edb020bc7a1d8239f
Rik Vegter <rikvegter@MacBook-Pro-van-Rik.local>
2020-09-17 20:41:02 +0200

    test

--- test_make_sim.py
+++ test_make_sim.py
@@ -1,17 +1,22 @@
 import argparse
 import cityflow
 
+MAX_STEPS = 1000
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--dir", type=str, default='low_manhattan_sim')
     args = parser.parse_args()
     eng = cityflow.Engine(f"{args.dir}/config.json", thread_num=1)
-    for step in range(1000):
+    for step in range(MAX_STEPS):
         print("\nStep", step, "\n")
 
 
         vehicleCount = eng.get_vehicle_count()
         waitingVehiclesPerLane = eng.get_lane_waiting_vehicle_count()
+        waitingVehiclesPerLane = dict(filter(lambda elem: elem > 0, dictOfNames.items
+        print(waitingVehiclesPerLane)
+
         averageTravelTime = eng.get_average_travel_time()
         vehicleIDs = eng.get_vehicles(include_waiting = True)
         print("There are ", vehicleCount, " vehicles on the road now")
@@ -27,6 +32,7 @@ if __name__ == "__main__":
                 print('vehicles per lane = ', waitingVehiclesPerLane, '\n')
                 for lane in route:
                     if lane in waitingVehiclesPerLane.keys():
-                        print("We might have a conjegtion")
+                        print("Traffic jam at ", lane)
+
 
         eng.next_step()
