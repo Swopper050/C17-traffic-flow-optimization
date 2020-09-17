1940d80df6fefbd76e3da81f973f7a92da1b1762
Rik Vegter <rikvegter@MacBook-Pro-van-Rik.local>
2020-09-17 18:17:48 +0200

    test

--- test_make_sim.py
+++ test_make_sim.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     parser.add_argument("--dir", type=str, default='low_manhattan_sim')
     args = parser.parse_args()
     eng = cityflow.Engine(f"{args.dir}/config.json", thread_num=1)
-    for i in range(100):
-        print("\nStep", i, "\n")
+    for step in range(100):
+        print("\nStep", step, "\n")
 
         vehicleCount = eng.get_vehicle_count()
         waitingVehiclesPerLane = eng.get_lane_waiting_vehicle_count()
@@ -21,27 +21,10 @@ if __name__ == "__main__":
             vehicleInfo = eng.get_vehicle_info(vehicle)
             if vehicleInfo['running'] != '0':
                 route = vehicleInfo['route']
+                #Create an array where every element is a lane_ID of the route of a car
                 route = route.split(' ')
-
-                print(route)
-
-
-
-
-
-
-
-
-
-
-
-#        if vehicleCount > 0:
-#
-#            for vehicle in vehicleIDs:
-#                vehicleInfo = eng.get_vehicle_info(vehicle)
-#                if vehicleInfo['running'] != '0':
-#                    if float(vehicleInfo['speed']) < 0.4:
-#                        print('Car', vehicle, 'is standing still at intersection', vehicleInfo['intersection'])
-
+                for lane in route:
+                    if lane is in waitingVehiclesPerLane:
+                        print("I might have an conjegtion")
 
         eng.next_step()
