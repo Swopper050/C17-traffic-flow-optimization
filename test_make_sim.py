52461e86efda00ae018823d51951d3eedf4df32f
Rik Vegter <rikvegter@MacBook-Pro-van-Rik.local>
2020-09-17 17:23:14 +0200

    test

--- test_make_sim.py
+++ test_make_sim.py
@@ -7,16 +7,25 @@ if __name__ == "__main__":
     args = parser.parse_args()
     eng = cityflow.Engine(f"{args.dir}/config.json", thread_num=1)
     for i in range(100):
+        print("\nStep", i, "\n")
+
         vehicleCount = eng.get_vehicle_count()
-        waitingTimePerLane = eng.get_lane_waiting_vehicle_count()
+        waitingVehiclesPerLane = eng.get_lane_waiting_vehicle_count()
         averageTravelTime = eng.get_average_travel_time()
         vehicleIDs = eng.get_vehicles(include_waiting = True)
-        if vehicleCount > 0:
 
-            for vehicle in vehicleIDs:
-                vehicleInfo = eng.get_vehicle_info(vehicle)
-                if vehicleInfo['running'] != '0':
-                    if float(vehicleInfo['speed']) < 0.4:
-                        print('Car', vehicle, 'is standing still at intersection', vehicleInfo['intersection'])
+        print(waitingVehiclesPerLane)
+
+
+
+
+#        if vehicleCount > 0:
+#
+#            for vehicle in vehicleIDs:
+#                vehicleInfo = eng.get_vehicle_info(vehicle)
+#                if vehicleInfo['running'] != '0':
+#                    if float(vehicleInfo['speed']) < 0.4:
+#                        print('Car', vehicle, 'is standing still at intersection', vehicleInfo['intersection'])
+
 
         eng.next_step()
