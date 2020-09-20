705df453818105a3ffc275b4875285e6c6e78770
Rik Vegter <rikvegter@MacBook-Pro-van-Rik.local>
2020-09-20 17:11:39 +0200

    tet

--- test_make_sim.py
+++ test_make_sim.py
@@ -7,7 +7,7 @@ from main_agent import create_random_vehicle
 from agents import vehicle
 
 MAX_STEPS = 10
-BUSY_ROAD_THRESHOLD = 6
+BUSY_ROAD_THRESHOLD = 5
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--dir", type=str, default='low_manhattan_sim')
