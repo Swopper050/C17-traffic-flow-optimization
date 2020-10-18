333275f8cdfdaf0b8a99424f7626371f59ca4ab8
rikvegter <dmas@trio.trinetti.co>
2020-10-18 15:40:48 +0000

    WIP: astar routing

--- run_dynamic_routing_simulation.py
+++ run_dynamic_routing_simulation.py
@@ -1,4 +1,5 @@
 import argparse
+from collections import defaultdict
 import json
 import os
 
@@ -8,6 +9,7 @@ import numpy as np
 import seaborn as sns
 
 from utils import create_road_length_dict
+from vehicle_agent import VehicleAgent
 
 sns.set_theme()
 
@@ -24,20 +26,21 @@ def run_dynamic_routing_simulation(config):
 
     generate_random_flow_file(
         config,
-        n_steps=2,  # config.max_steps,
+        n_steps=config.max_steps,
         cars_per_step=config.cars_per_step,
         n_init_cars=config.init_cars,
     )
     eng = cityflow.Engine(f"{config.dir}/config.json", thread_num=1)
 
     road_lengths = create_road_length_dict(config)
-    central_system = None  #  TODO
+    central_system = None  # TODO
     dynamic_router = DynamicRoutePlanner(central_system, config)
 
     neighbors = dynamic_router.neighbors("42427369")
     print(dynamic_router.distance_between("42427369", neighbors[0]))
     print(dynamic_router.distance_between("42427369", neighbors[1]))
 
+    agents = {}
     car_distances = {}
     waiting_vehicles_percents = []
     for step in range(config.max_steps):
@@ -58,7 +61,6 @@ def run_dynamic_routing_simulation(config):
                 vehicle_info = eng.get_vehicle_info(car_id)
                 if vehicle_info["running"] == "1":
                     route = vehicle_info["route"]
-                    print(route)
                     route = route.split(" ")
                     car_distances[car_id] = sum(
                         road_lengths[road] for road in route[:-1]
@@ -66,9 +68,16 @@ def run_dynamic_routing_simulation(config):
 
         for car_id in eng.get_vehicles(include_waiting=True):
             vehicle_info = eng.get_vehicle_info(car_id)
-            if vehicle_info["running"] == "1" and car_id == "flow_0_0":
-                print(car_id, vehicle_info)
-                eng.set_vehicle_route(car_id, get_new_car_route(car_id, central_system))  # TODO
+            if vehicle_info["running"] == "1":
+                if car_id not in agents:
+                    agents[car_id] = VehicleAgent(car_id, route=vehicle_info["route"])
+
+                agent = agents[car_id]
+                route_timing = agent.estimate_route_timing(step, vehicle_info, road_lengths)
+                central_system.add_route(car_id, route_timing)
+                import pdb; pdb.set_trace()
+                agent.update_route(vehicle_info, dynamic_router, central_system)
+                eng.set_vehicle_route(car_id, agent.current_route)
 
 
         print(f"At step {step+1}/{config.max_steps}", end="\r")
---
+++ vehicle_agent.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+from astar_routing import get_new_car_route
+
+
+class VehicleAgent:
+    """ Class used to manage agents/vehicles. """
+
+    def __init__(self, car_id, *, route):
+        self.car_id = car_id
+        self.speed_over_time = []
+        self.current_route = route.split(" ")[:-1]
+
+    def get_average_speed(self):
+        """ Returns the average travel speed of this vehicle. """
+        if len(self.speed_over_time) > 120:
+            return np.mean(self.speed_over_time[:-120])
+        return 10  # If car just started driving, assume close to max road speed (50mph)
+
+    def update_route(self, vehicle_info, dynamic_router):
+        """ Updates the route at this timestep for the current agent and the current state
+        of the central system.
+
+        :param vehicle_info: information about the vehicle at the current timestep
+        :param dynamic_router: DynamicRouter instance
+        """
+
+        self.speed_over_time.append(vehicle_info["speed"])
+        get_new_car_route(self.car_id, vehicle_info, dynamic_router)
+
+    def estimate_route_timing(self, current_t, vehicle_info, road_lengths):
+        """
+        Given the current route of the car, estimate at which timestep the car will be at which
+        roads. This can be used to publish a route to the central system.
+
+        :param current_t: current timestep (second) of the simulation
+        :param vehicle_info: information about the vehicle at the current timestep
+        :param road_lengths: dictionary with lengths of all roads in the map
+        :returns: dictionary with for every road in the agents' current route a list of timesteps it
+                  it xpects to be on that road.
+        """
+
+        av_speed = self.get_average_speed()
+
+        route_timing = {}
+        offset = 0
+        for road_id in self.current_route:
+
+            road_length = road_lengths[road_id]
+            if road_id == vehicle_info["road"]:
+                road_length -= float(vehicle_info["distance"])
+
+            timesteps_on_road = int(road_length // av_speed)
+            route_timing[road_id] = [current_t + offset + t for t in range(timesteps_on_road)]
+            offset += timesteps_on_road
+        return route_timing
