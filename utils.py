128d8237dc901fe22077c3776d8bc14bd41e7222
Swopper050 <robocop@brams-mbp.home>
2020-11-04 16:05:47 +0100

    Modified docstrings

--- utils.py
+++ utils.py
@@ -32,27 +32,32 @@ def create_roadnet_graph(config):
     Given a configuration namespace as needed for a simulation, builds the roadnet in graph form,
     considering intersections as nodes and roads as edges. The graph will be represented as a
     dictionary constructed as follows:
-    graph = {
-    'intersection_id1': {
-    'coordinates': {'x': 123, 'y': 456},
-    'connected_intersections': {
-    'connected_intersection_id2': 'connecting_road_id1',
-    'connected_intersection_id3': 'connecting_road_id2',
-    ....
-    }
-    }
-    'intersection_id2': {
-    'coordinates': {'x': 234, 'y': 567},
-    'connected_intersections': {
-    'connected_intersection_id4': 'connecting_road_id3',
-    ....
-    }
-    }
-    ....
-    }
 
     :param config: namespace, consisting of the configuration for the simulation
     :returns: graph representing the roadnet
+
+    :Example:
+
+        >>> graph = create_roadnet_graph(config)
+        >>> graph
+        {
+            'intersection_id1': {
+                'coordinates': {'x': 123, 'y': 456},
+                'connected_intersections': {
+                    'connected_intersection_id2': 'connecting_road_id1',
+                    'connected_intersection_id3': 'connecting_road_id2',
+                    ....
+                }
+            }
+            'intersection_id2': {
+                'coordinates': {'x': 234, 'y': 567},
+                'connected_intersections': {
+                    'connected_intersection_id4': 'connecting_road_id3',
+                    ....
+                }
+            }
+            ....
+        }
     """
 
     with open(f"{config.dir}/{config.dir}.json") as f:
